a9c87c197dafd92a497bdc641684cc6a3cf81c0e
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-12 18:33:04 -0500

    Add header comment to file

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,11 @@
+# *****************************************************************************
+# Name: Jason D. F. D'Oyley
+# Date: November 12, 2022
+# Assignment: GP 3 - Task 1 - Hello World
+#
+# Task Description: Demonstrate writing and exporting text in Python.
+# *****************************************************************************
+
 print("jasdoy5736")
 print("Hello World, my name is Jason D'Oyley")
 print("I am a student at ECPI University")
